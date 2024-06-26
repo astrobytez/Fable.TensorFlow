003e83b63ae5a17d40cd47781b0fdd04c1e5f7e3
Nick Hallam <fscoder@proton.me>
2024-06-26 13:01:42 +1000

    Add project build + paket support.

---
+++ Build.fs
@@ -0,0 +1,42 @@
+﻿open Fake.Core
+
+open Fake.IO
+open Fake.Core
+open Fake.IO.FileSystemOperators
+open Fake.IO.Globbing.Operators
+open Fake.Core.TargetOperators
+
+[<Literal>]
+let here = __SOURCE_DIRECTORY__
+
+
+let initializeContext () =
+    let execContext = Context.FakeExecutionContext.Create false "build.fsx" []
+    Context.setExecutionContext (Context.RuntimeContext.Fake execContext)
+
+let runProc cmd path args =
+    CreateProcess.fromRawCommand cmd args
+    |> CreateProcess.withWorkingDirectory path
+    |> Proc.run // start with the above configuration
+    |> ignore // ignore exit code
+
+let dotnet args path = runProc "dotnet" path args
+
+initializeContext ()
+
+Target.create "Clean" (fun _ -> !! "src/**/bin" ++ "src/**/obj" |> Shell.cleanDirs)
+
+Target.create "Build" (fun _ ->
+    dotnet [ "build" ]  (here </> "fable")
+)
+
+Target.create "All" ignore
+
+let deps = [
+    "Clean" ==> "Build" ==> "All"
+]
+
+[<EntryPoint>]
+let main args =
+    Target.runOrDefault "All"
+    0
