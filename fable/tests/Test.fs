830ada83145000147a59892f78ef97b4aa5c5c1c
Nick Hallam <fscoder@proton.me>
2024-07-02 11:00:15 +1000

    Optimiser changes test outcome.
    Looks like the adam optimiser is poor choice for this example.

--- fable/tests/Test.fs
+++ fable/tests/Test.fs
@@ -18,10 +18,12 @@ let run () = async {
   let layer = Dense.props [ Units 1; InputShape [| 1 |] ]
   model.add (Tf.layers.dense layer)
 
-  model.compile (ModelCompileArgs.props [ Loss MeanSquaredError; Optimizer Adam ])
+  model.compile (ModelCompileArgs.props [ Loss MeanSquaredError; Optimizer StocasticGradientDecent ])
 
   let xs = Tf.tensor2d ([| -1.0; 0.0; 1.0; 2.0; 3.0; 4.0 |], [| 6; 1 |])
   let ys = Tf.tensor2d ([| -3.0; -1.0; 1.0; 3.0; 5.0; 7.0 |], [| 6; 1 |])
+
+  printfn "Fitting model"
 
   do! Async.AwaitPromise(model.fit (xs, ys, {| epochs = 250 |}))
   return model
@@ -32,7 +34,7 @@ let coreTests =
 
         testAsync "Test - 1" {
             let! value = run ()
-            value.predict(Tf.tensor2d ([| 6.0 |], [| 1; 1 |], Float32)).print ()
+            value.predict(Tf.tensor2d ([| 20.0 |], [| 1; 1 |])).print ()
             Expect.passWithMsg $"Tensorflow completed successfully"
         }
     ]
